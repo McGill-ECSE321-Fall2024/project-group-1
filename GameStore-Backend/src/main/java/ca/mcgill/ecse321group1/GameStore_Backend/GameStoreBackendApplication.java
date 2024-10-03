e61cef25c57557824dabc8ff8fa89a0baaf7b27c
AlanBrotherton <143669642+AlanBrotherton@users.noreply.github.com>
2024-10-03 15:10:40 -0400

    Added GameStore-Backend from Spring Boot

---
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321group1/GameStore_Backend/GameStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321group1.GameStore_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreBackendApplication.class, args);
+	}
+
+}
---
+++ GameStore-Backend/src/test/java/ca/mcgill/ecse321group1/GameStore_Backend/GameStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321group1.GameStore_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
