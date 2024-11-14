dba8497c5dd942fe1a64f0c5399b0155135e808c
1vanzhang <ivan.zhang2@mail.mcgill.ca>
2024-11-13 19:50:36 -0500

    wip

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321group1/gamestore/controller/StaffController.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321group1/gamestore/controller/StaffController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import jakarta.validation.Valid;
+
 import ca.mcgill.ecse321group1.gamestore.service.StaffService;
 import ca.mcgill.ecse321group1.gamestore.dto.PersonResponseDto;
 import ca.mcgill.ecse321group1.gamestore.dto.PersonRequestDto;
+import ca.mcgill.ecse321group1.gamestore.model.Staff;
 
 
 @RestController
@@ -24,7 +27,8 @@ public class StaffController {
      * 
      */
     @PostMapping("/staff")
-    public PersonResponseDto createStaff(@Valid @RequestBody PersonRequestDto) {
-
+    public PersonResponseDto createStaff(@Valid @RequestBody PersonRequestDto personToCreate) {
+        Staff createdPerson = staffService.createStaff(personToCreate.getUsername(), personToCreate.getEmail(), personToCreate.getPasswordHash());
+        return new PersonResponseDto(createdPerson);
     }
 }
