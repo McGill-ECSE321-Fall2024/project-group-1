e154f529245e57018bd561352591339cd57af93b
1vanzhang <ivan.zhang2@mail.mcgill.ca>
2024-11-16 23:54:08 -0500

    Finished more controllers

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321group1/gamestore/controller/ReplyController.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321group1/gamestore/controller/ReplyController.java
@@ -1,6 +1,26 @@
 package ca.mcgill.ecse321group1.gamestore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321group1.gamestore.model.Reply;
+import ca.mcgill.ecse321group1.gamestore.service.ReplyService;
+
+@RestController
 public class ReplyController {
-    
+    @Autowired
+    private ReplyService replyService;
+
+    /**
+     * Get reply object by ID
+     * @param rid
+     */
+    @GetMapping("/reply/{rid}")
+    public Reply getReplyById(@PathVariable int rid) {
+        return replyService.getReply(rid);
+    }
     
 }
